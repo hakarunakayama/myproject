aa2a3fbf7046c11b7a1f6ab02bf4b1420a9181d8
Hakaru Nakayama <hakarunakayama@yahoo.ne.jp>
2024-01-26 21:51:30 +0900

    urls.pyのfrom crudを..crudに修正

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from crud import views
+from ..crud import views
 from django.conf import settings
 from django.conf.urls.static import static
 
